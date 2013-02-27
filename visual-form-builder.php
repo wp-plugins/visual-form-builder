647805cbbc0b5f4de50466997e068df5ce2f894c
mmuro <mmuro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-27 14:37:17 +0000

    Add dashboard widget for displaying recent entries
    git-svn-id: https://plugins.svn.wordpress.org/visual-form-builder/trunk@674078 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- visual-form-builder.php
+++ visual-form-builder.php
@@ -4,7 +4,7 @@ Plugin Name: Visual Form Builder
 Description: Dynamically build forms using a simple interface. Forms include jQuery validation, a basic logic-based verification system, and entry tracking.
 Author: Matthew Muro
 Author URI: http://matthewmuro.com
-Version: 2.6.9
+Version: 2.7
 */
 
 /*
@@ -82,6 +82,7 @@ class Visual_Form_Builder{
 			// Build options and settings pages.
 			add_action( 'admin_menu', array( &$this, 'add_admin' ) );
 			add_action( 'admin_init', array( &$this, 'save' ) );
+			add_action( 'admin_init', array( &$this, 'additional_plugin_setup' ) );
 			
 			add_action( 'wp_ajax_visual_form_builder_process_sort', array( &$this, 'process_sort_callback' ) );
 			add_action( 'wp_ajax_visual_form_builder_create_field', array( &$this, 'create_field_callback' ) );
@@ -94,6 +95,9 @@ class Visual_Form_Builder{
 
 			// Adds additional media button to insert form shortcode
 			add_action( 'media_buttons', array( &$this, 'add_media_button' ), 999 );
+			
+			// Adds a Dashboard widget
+			add_action( 'wp_dashboard_setup', array( &$this, 'add_dashboard_widget' ) );
 			
 			// Load the includes files
 			add_action( 'load-visual-form-builder_page_vfb-entries', array( &$this, 'includes' ) );
@@ -132,6 +136,23 @@ class Visual_Form_Builder{
 		
 		// Add CSS to the front-end
 		add_action( 'wp_enqueue_scripts', array( &$this, 'css' ) );
+	}
+
+	/**
+	 * Allow for additional plugin code to be run during admin_init
+	 * which is not available during the plugin __construct()
+	 * 
+	 * @since 2.7
+	 */
+	public function additional_plugin_setup() {
+			
+		if ( !get_option( 'vfb_dashboard_widget_options' ) ) {
+			$widget_options['vfb_dashboard_recent_entries'] = array(
+				'items' => 5,
+			);
+			update_option( 'vfb_dashboard_widget_options', $widget_options );
+		}
+			
 	}
 	
 	/**
@@ -224,6 +245,92 @@ class Visual_Form_Builder{
 		</div>
 	<?php
 		die(1);
+	}
+	
+	/**
+	 * Adds the dashboard widget
+	 * 
+	 * @since 2.7
+	 */
+	public function add_dashboard_widget() {
+		wp_add_dashboard_widget( 'vfb-dashboard', __( 'Recent Visual Form Builder Entries', 'visual-form-builder' ), array( &$this, 'dashboard_widget' ), array( &$this, 'dashboard_widget_control' ) );
+	}
+	
+	/**
+	 * Displays the dashboard widget content
+	 * 
+	 * @since 2.7
+	 */
+	public function dashboard_widget() {
+		global $wpdb;
+		
+		// Get the date/time format that is saved in the options table
+		$date_format = get_option( 'date_format' );
+		$time_format = get_option( 'time_format' );
+		
+		$widgets = get_option( 'vfb_dashboard_widget_options' );
+		$total_items = isset( $widgets['vfb_dashboard_recent_entries'] ) && isset( $widgets['vfb_dashboard_recent_entries']['items'] ) ? absint( $widgets['vfb_dashboard_recent_entries']['items'] ) : 5;
+		
+		$entries = $wpdb->get_results( $wpdb->prepare( "SELECT forms.form_title, entries.entries_id, entries.form_id, entries.sender_name, entries.sender_email, entries.date_submitted FROM $this->form_table_name AS forms INNER JOIN $this->entries_table_name AS entries ON entries.form_id = forms.form_id ORDER BY entries.date_submitted DESC LIMIT %d", $total_items ) );
+						
+		if ( !$entries ) :
+			echo sprintf(
+				'<p>%1$s<a href="%2$s">%3$s</a>.',
+				__( 'You currently do not have any forms.', 'visual-form-builder' ),
+				esc_url( admin_url( 'admin.php?page=vfb-add-new' ) ),
+				__( 'Get started!', 'visual-form-builder' )
+			);
+		else :
+			
+			$content = '';
+			
+			foreach ( $entries as $entry ) :
+				
+				$content .= sprintf(
+					'<li><a href="%1$s">%4$s</a> via <a href="%2$s">%5$s</a> <span class="rss-date">%6$s</span><cite>%3$s</cite></li>',
+					esc_url( add_query_arg( array( 'action' => 'view', 'entry' => absint( $entry->entries_id ) ), admin_url( 'admin.php?page=vfb-entries' ) ) ),
+					esc_url( add_query_arg( 'form-filter', absint( $entry->form_id ), admin_url( 'admin.php?page=vfb-entries' ) ) ),
+					esc_html( $entry->sender_name ),
+					esc_html( $entry->sender_email ),
+					esc_html( $entry->form_title ),
+					date( "$date_format $time_format", strtotime( $entry->date_submitted ) )
+				);
+				
+			endforeach;
+			
+			echo "<div class='rss-widget'><ul>$content</ul></div>";
+		
+		endif;
+	}
+	
+	/**
+	 * Displays the dashboard widget form control
+	 * 
+	 * @since 2.7
+	 */
+	public function dashboard_widget_control() {
+		if ( !$widget_options = get_option( 'vfb_dashboard_widget_options' ) )
+			$widget_options = array();
+	
+		if ( !isset( $widget_options['vfb_dashboard_recent_entries'] ) )
+			$widget_options['vfb_dashboard_recent_entries'] = array();
+	
+		if ( 'POST' == $_SERVER['REQUEST_METHOD'] && isset( $_POST['vfb-widget-recent-entries'] ) ) {
+			$number = absint( $_POST['vfb-widget-recent-entries']['items'] );
+			$widget_options['vfb_dashboard_recent_entries']['items'] = $number;
+			update_option( 'vfb_dashboard_widget_options', $widget_options );
+		}
+	
+		$number = isset( $widget_options['vfb_dashboard_recent_entries']['items'] ) ? (int) $widget_options['vfb_dashboard_recent_entries']['items'] : '';
+	
+		echo sprintf(
+			'<p>
+			<label for="comments-number">%1$s</label>
+			<input id="comments-number" name="vfb-widget-recent-entries[items]" type="text" value="%2$d" size="3" />
+			</p>',
+			__( 'Number of entries to show:', 'visual-form-builder' ),
+			$number
+		);
 	}
 		
 	/**
