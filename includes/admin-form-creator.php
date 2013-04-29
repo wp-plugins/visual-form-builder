cf8581d7c64ae3f850c32b129f5283fe3f13efef
mmuro <mmuro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-29 21:30:54 +0000

    Simplify strings for Settings and Action buttons
    git-svn-id: https://plugins.svn.wordpress.org/visual-form-builder/trunk@705642 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin-form-creator.php
+++ includes/admin-form-creator.php
@@ -91,11 +91,11 @@ $page_main = $this->_admin_pages[ 'vfb' ];
 									?>
 			                                
 	                                <div class="vfb-button-group">
-										<a href="#form-settings" id="form-settings-button" class="vfb-button vfb-first <?php echo $opened_tab; ?>"><?php _e( 'Form Settings' , 'visual-form-builder'); ?><span class="button-icon vfb-small-arrow"></span></a>
-	                                    <a href="<?php echo esc_url( wp_nonce_url( admin_url('admin.php?page=visual-form-builder&amp;action=copy_form&amp;form=' . $form_nav_selected_id ), 'copy-form-' . $form_nav_selected_id ) ); ?>" class="vfb-button vfb-duplicate"><?php _e( 'Duplicate Form' , 'visual-form-builder'); ?><span class="button-icon plus"></span></a>
-	                                    <a href="<?php echo esc_url( wp_nonce_url( admin_url('admin.php?page=visual-form-builder&amp;action=delete_form&amp;form=' . $form_nav_selected_id ), 'delete-form-' . $form_nav_selected_id ) ); ?>" class="vfb-button vfb-delete vfb-last menu-delete"><?php _e( 'Delete Form' , 'visual-form-builder'); ?><span class="button-icon delete"></span></a>
+										<a href="#form-settings" id="form-settings-button" class="vfb-button vfb-first <?php echo $opened_tab; ?>"><?php _e( 'Settings' , 'visual-form-builder'); ?><span class="button-icon vfb-small-arrow"></span></a>
+	                                    <a href="<?php echo esc_url( wp_nonce_url( admin_url('admin.php?page=visual-form-builder&amp;action=copy_form&amp;form=' . $form_nav_selected_id ), 'copy-form-' . $form_nav_selected_id ) ); ?>" class="vfb-button vfb-duplicate"><?php _e( 'Duplicate' , 'visual-form-builder'); ?><span class="button-icon plus"></span></a>
+	                                    <a href="<?php echo esc_url( wp_nonce_url( admin_url('admin.php?page=visual-form-builder&amp;action=delete_form&amp;form=' . $form_nav_selected_id ), 'delete-form-' . $form_nav_selected_id ) ); ?>" class="vfb-button vfb-delete vfb-last menu-delete"><?php _e( 'Delete' , 'visual-form-builder'); ?><span class="button-icon delete"></span></a>
 	                                    
-	                                    <?php submit_button( __( 'Save Form', 'visual-form-builder' ), 'primary', 'save_form', false ); ?>
+	                                    <?php submit_button( __( 'Save', 'visual-form-builder' ), 'primary', 'save_form', false ); ?>
 	                                </div>
 			                                                                            
 		                                <div id="form-settings" class="<?php echo $opened_tab; ?>">
