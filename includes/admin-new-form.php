83f977092128cd02255a5fb684d87d9257a12aa0
mmuro <mmuro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-15 14:28:05 +0000

    Remove form name placeholder when creating a new form
    git-svn-id: https://plugins.svn.wordpress.org/visual-form-builder/trunk@741071 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin-new-form.php
+++ includes/admin-new-form.php
@@ -2,19 +2,19 @@
 	<input name="action" type="hidden" value="create_form" />
     <?php
     	wp_nonce_field( 'create_form' );
-    	
+
     	if ( !current_user_can( 'manage_options' ) )
     		wp_die( __( 'You do not have sufficient permissions to create a new form.', 'visual-form-builder' ) );
     ?>
 	<h3><?php _e( 'Create a form' , 'visual-form-builder'); ?></h3>
-	
+
 	<table class="form-table">
 		<tbody>
 			<!-- Form Name -->
 			<tr valign="top">
 				<th scope="row"><label for="form-name"><?php _e( 'Name the form' , 'visual-form-builder'); ?></label></th>
 				<td>
-					<input type="text" value="<?php _e( 'Enter form name here' , 'visual-form-builder'); ?>" placeholder="<?php _e( 'Enter form name here' , 'visual-form-builder'); ?>" autofocus="autofocus" onfocus="this.select();" class="regular-text required" id="form-name" name="form_title" />
+					<input type="text" autofocus="autofocus" class="regular-text required" id="form-name" name="form_title" />
 					<p class="description"><?php _e( 'Required. This name is used for admin purposes.' , 'visual-form-builder'); ?></p>
 				</td>
 			</tr>
@@ -51,7 +51,7 @@
 					<p class="description"><?php _e( 'Optional (for now) - you can change this later' , 'visual-form-builder'); ?></p>
 				</td>
 			</tr>
-			
+
 		</tbody>
 	</table>
 	<?php submit_button( __( 'Create Form', 'visual-form-builder' ) ); ?>
